ac81f719ad844fb87dbb6bdda35af77f5ddf0dff
nt189 <antonio.moralesba@alumno.buap.mx>
2025-04-28 21:47:30 -0600

    Interfaz de usuarios conectados en chats grupales

---
+++ Animations.js
@@ -0,0 +1,31 @@
+document.getElementById("toggle-sidebar").addEventListener("click", function () {
+    const body = document.body;
+    const sidebar = document.getElementById("right-sidebar");
+    const toggleButton = document.getElementById("toggle-sidebar");
+
+    // Alternar la clase 'active' en el sidebar
+    sidebar.classList.toggle("active");
+
+    // Alternar la clase 'sidebar-active' en el body
+    if (sidebar.classList.contains("active")) {
+        body.classList.add("sidebar-active");
+        toggleButton.style.display = "none"; // Ocultar el botón toggle-sidebar
+    } else {
+        body.classList.remove("sidebar-active");
+    }
+});
+
+document.getElementById("close-sidebar").addEventListener("click", function () {
+    const body = document.body;
+    const sidebar = document.getElementById("right-sidebar");
+    const toggleButton = document.getElementById("toggle-sidebar");
+
+    // Eliminar la clase 'active' del sidebar
+    sidebar.classList.remove("active");
+
+    // Eliminar la clase 'sidebar-active' del body
+    body.classList.remove("sidebar-active");
+
+    // Mostrar el botón toggle-sidebar
+    toggleButton.style.display = "block";
+});
